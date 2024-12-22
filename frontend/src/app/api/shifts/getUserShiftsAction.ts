1832a3c5f61c580ba1f0cb0e5491266f9ebae54a
Vinicius Shiryu Hagio <viniciusshiryu@outlook.com>
2024-12-22 16:15:22 -0300

    debug

--- frontend/src/app/api/shifts/getUserShiftsAction.ts
+++ frontend/src/app/api/shifts/getUserShiftsAction.ts
@@ -4,7 +4,9 @@ export const getUserShiftsAction = async (userId: number) => {
   const apiHost = process.env.API_HOST || "http://localhost:3001";
 
   try {
+    console.log("get users shift action")
     const response = await axios.get(`${apiHost}/shifts/user/${userId}`);
+    console.log(response.data)
     return response.data;
   } catch (error) {
     console.error("Erro ao carregar turnos:", error);
--- frontend/src/app/api/shifts/startShiftAction.ts
+++ frontend/src/app/api/shifts/startShiftAction.ts
@@ -3,9 +3,11 @@ import axios from "axios";
 export async function startShiftAction(userId: number) {
   const apiHost = process.env.API_HOST || "http://localhost:3001";
   try {
+    console.log("start shift action")
     const response = await axios.post(`${apiHost}/shifts`, {
       user_id: userId,
     });
+    console.log(response.data)
 
     return response.data;
   } catch (error: unknown) {
